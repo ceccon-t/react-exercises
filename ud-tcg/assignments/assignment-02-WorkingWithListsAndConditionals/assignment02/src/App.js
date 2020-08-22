9cd6c8f275401046f95e9c4cc9cae21653de630e
Tiago Ceccon <ceccon.t@hotmail.com>
2020-08-21 23:35:59 -0300

    Add input field with listener to output text length

--- ud-tcg/assignments/assignment-02-WorkingWithListsAndConditionals/assignment02/src/App.js
+++ ud-tcg/assignments/assignment-02-WorkingWithListsAndConditionals/assignment02/src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    currentText: ""
+  }
+
+  textChangedHandler = (event) => {
+    let newText = event.target.value;
+    this.setState({
+      currentText: newText
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Assignment 02</h1>
+        <br />
+        <input onChange={this.textChangedHandler} value={this.state.currentText}/>
+        <p>Length: {this.state.currentText.length}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
