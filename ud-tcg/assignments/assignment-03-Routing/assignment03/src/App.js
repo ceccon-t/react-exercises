02e533017a2d4f602355a5c6aa8af2a363f8b330
Tiago Ceccon <ceccon.t@hotmail.com>
2020-09-20 18:17:04 -0300

    Load course component by clicking course

--- ud-tcg/assignments/assignment-03-Routing/assignment03/src/App.js
+++ ud-tcg/assignments/assignment-03-Routing/assignment03/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 
 import Courses from './containers/Courses/Courses';
+import Course from './containers/Course/Course';
 import Users from './containers/Users/Users';
 
 class App extends Component {
@@ -11,7 +12,7 @@ class App extends Component {
         <ol style={{textAlign: 'left'}}>
           <li><del>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</del></li>
           <li><del>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</del></li>
-          <li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
+          <li><del>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</del></li>
           <li>Pass the course ID to the "Course" page and output it there</li>
           <li>Pass the course title to the "Course" page - pass it as a param or score bonus points by passing it as query params (you need to manually parse them though!)</li>
           <li>Load the "Course" component as a nested component of "Courses"</li>
@@ -30,6 +31,7 @@ class App extends Component {
             </nav>
             <Switch>
               <Route path="/courses" component={Courses} />
+              <Route path="/course" component={Course} />
               <Route path="/users" component={Users} />
             </Switch>
           </div>
